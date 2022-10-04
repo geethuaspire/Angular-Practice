2dc662c481ede5cb7c7c9b8653e1e49704c36bec
geethu_aspire <geethu.gopi@aspiresys.com>
2022-10-04 18:43:38 +0530

    nebular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,37 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+// import { BrowserModule } from '@angular/platform-browser';
+// import { NgModule } from '@angular/core';
+// import { AppComponent } from './app.component';
+// import { AppRoutingModule } from './app-routing.module';
+// //import { DragulaModule } from 'ng2-dragula';
+// //import { MomentModule } from 'ngx-moment';
+// //import { NgPipesModule } from 'ngx-pipes';
+// import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+// //import {
+//  // NbIconModule,
+//  // NbUserModule,
+//  // NbButtonModule, NbBadgeModule, NbThemeModule, NbSelectModule, NbCardModule, NbLayoutModule, NbContextMenuModule, NbActionsModule, NbDialogModule, NbMenuModule
+// //} from '@nebular/theme';
+// //import { TaskboardComponent } from './taskboard/taskboard.component';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+// @NgModule({
+//   declarations: [
+//     AppComponent,
 
-@NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
-})
-export class AppModule { }
+//   ]
+//  /* imports: [
+//    BrowserModule, AppRoutingModule,
+//     BrowserAnimationsModule,
+//     NbThemeModule.forRoot({ name: 'gdpr' }),
+//     NbLayoutModule,
+//     NbActionsModule,
+
+//     DragulaModule.forRoot(),
+//     NbContextMenuModule,
+   
+
+//   ],
+//   providers: [],
+//   bootstrap: [AppComponent]
+// })
+// export class AppModule { }
+// })
