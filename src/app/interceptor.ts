a826f5e620d2d815c5a5c32e2cf25e825c257ad8
geethu_aspire <geethu.gopi@aspiresys.com>
2022-10-04 16:03:35 +0530

    add Exemptions

--- src/app/interceptor.ts
+++ src/app/interceptor.ts
@@ -11,31 +11,94 @@ import { Observable } from 'rxjs';
 
 import { SigninService } from './signin.service';
 
-const EXEMPTIONS: any = [];
+const EXEMPTIONS: any = {
+  'account-user-role-id': [
+    'views/',
+    'i18n/',
+    '/site/register',
+    '/site/verify',
+    '/countries',
+    '/industries',
+    '/timezones',
+    '/years',
+    '/counts',
+    '/site/login',
+    '/site/adminlogin',
+    '/site/recover',
+    '/site/reset',
+    '/reminders',
+    '/code',
+    '/user/userupdate',
+    '/site/activate',
+    '/users/details',
+  ],
+
+  auth: [
+    'views/',
+    'i18n/',
+    '/site/register',
+    '/site/verify',
+    '/site/lang',
+    '/site/login',
+    '/site/adminlogin',
+    '/site/recover',
+    '/site/reset',
+    '/site/activate',
+    '/site/integratemesser',
+  ],
+
+  lan: ['views/', 'i18n/'],
+};
+
 @Injectable()
 export class AuthHeaderInterceptor implements HttpInterceptor {
+  // var currentUserAccountRole = BrowserStorageService.user.accountRoleID.value;
+  //     var authKey = BrowserStorageService.user.authKey.value;
+  //     var currentLanguageId = LanguageService.getCurrentLanguage().id + 1;
   constructor(private signinservice: SigninService) {}
+
   intercept(
     req: HttpRequest<any>,
     next: HttpHandler
   ): Observable<HttpEvent<any>> {
     const { url } = req;
-    if (this.shouldIntercept(url)) {
+
+    if (!this.isUrlInArray(req.url, EXEMPTIONS['auth'])) {
       req = req.clone({
-        // headers: req.headers.append(
-        // 'Authorization',
-        // `Bearer ${this.signinservice.getAuthToken()}`
-        // ),
+        setHeaders: {
+          //'Authorization': `Bearer ${this.signinservice.getAuthToken()}`,
+        },
+      });
+    }
+
+    if (!this.isUrlInArray(req.url, EXEMPTIONS['accountid'])) {
+      req = req.clone({
+        setHeaders: {
+          'account-user-role-id': 'currentUserAccountRole',
+        },
+      });
+    }
+
+    if (!this.isUrlInArray(req.url, EXEMPTIONS('lan'))) {
+      req = req.clone({
+        setHeaders: {
+          lan: 'currentLanguageId',
+        },
       });
     }
     return next.handle(req);
   }
-  private shouldIntercept(url: string) {
-    for (const exemption of EXEMPTIONS) {
-      if (url.endsWith(exemption)) {
-        return false;
+
+  isUrlInArray(url: string, arr: any) {
+    var index;
+
+    for (index in arr) {
+      if (url.search(arr[index]) !== -1) {
+        return true;
       }
     }
-    return true;
+
+    return false;
   }
 }
+
